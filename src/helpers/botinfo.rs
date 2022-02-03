cea132dcbc6a889144ef47ffe258c0851870ac51
Owen Flaherty <thatsimplekid@gmail.com>
2022-02-03 15:52:43 +0000

    feat: added more stuff

---
+++ src/helpers/botinfo.rs
@@ -0,0 +1,50 @@
+use std::{env, process};
+use serenity::{
+    client::{bridge::gateway::ShardId, Context},
+    framework::standard::CommandResult,
+};
+use tokio::process::Command;
+
+pub async fn get_last_commit(ctx: &Context) -> Result<CommitResponse, Box<dyn std::error::Error + Send + Sync>> {
+    let reqwest_client = ctx.data.read().await.get::<ReqwestClient>().cloned().unwrap();
+    let resp = reqwest_client
+        .get("https://api.github.com/repos/thatsimplekid/QuiVive/commits/master")
+        .send().await?.json::<CommitResponse>().await?;
+    Ok(resp)
+}
+
+pub async fn get_system_info(ctx: &Context) -> CommandResult<SysInfo> {
+    let shard_manager = ctx.data.read().await.get::<ShardManagerContainer>().cloned().unwrap();
+    let shard_latency = {
+        let manager = shard_manager.lock().await;
+        let runners = manager.runners.lock().await;
+        let runner_raw = runners.get(&ShardId(ctx.shard_id));
+        match runner_raw {
+            Some(runner) => match runner.latency {
+                Some(ms) => format!("{}ms", ms.as_millis()),
+                None => "?ms".to_string(),
+            },
+            None => "?ms".to_string(),
+        }
+    };
+    let pid = process::id();
+    let raw_bin_path = env::current_exe()?;
+    let bin_path = raw_bin_path.to_string_lossy();
+    let bin_str = bin_path.rsplit('/').next().unwrap();
+    let mem_stdout = Command::new("sh")
+        .arg("-c")
+        .arg(format!(
+            "pmap {} | grep {} | awk 'NR>1 {{sum+=substr($2, 1, length($2)-1)}} END {{print sum}}'",
+            pid, bin_str
+        ))
+        .output()
+        .await
+        .expect("failed to get memory usage");
+    let mem_used = String::from_utf8(mem_stdout.stdout).unwrap();
+    let memory = &mem_used[..mem_used.len() - 1].parse::<f32>().unwrap() / 1000f32;
+    let sys_info = SysInfo {
+        shard_latency,
+        memory,
+    };
+    Ok(sys_info)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,72 @@
-fn main() {
-    println!("Hello, world!");
+mod helpers;
+
+use dashmap::DashMap;
+use reqwest::Client as Reqwest;
+use serenity::{client::bridge::gateway::GatewayIntents, http::Http, Client};
+use std::{
+    collections::{HashMap, HashSet},
+    env,
+    sync::{atomic::AtomicBool, Arc},
+};
+use tracing::{error, info};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    tracing_subscriber::fmt::init();
+    let args: Vec<String> = env::args().collect();
+    let creds = helpers::credentials_helper::read_creds(args[1].to_owned()).unwrap();
+    let token = &creds.bot_token;
+    let http = Http::new_with_token(token);
+    let (owners, bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            owners.insert(info.owners.id);
+            (owners, info.id)
+        },
+        Err(why) => panic!("Failed to access application info: {:?}", why),
+    };
+    let pool = database_helper::obtain_db_pool(creds.db_connection).await?;
+    let prefixes = database_helper::fetch_prefixes(&pool).await?;
+    let reqwest_client = Reqwest::builder()
+        .user_agent("Mozilla/5.0 (X11; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0")
+        .build()?;
+    let mut pub_creds = HashMap::new();
+    pub_creds.insert("default prefix".to_owned(), creds.default_prefix);
+    let emergency_commands = command_utils::get_allowed_commands();
+    let mut client = Client::builder(&token)
+        .framework(get_framework(bot_id, owners).await)
+        .event_handler(SerenityHandler {
+            run_loop: AtomicBool::new(true),
+        })
+        .intents({
+            GatewayIntents::GUILD_MEMBERS           |
+            GatewayIntents::GUILD_PRESENCES         |
+            GatewayIntents::GUILD_BANS              |
+            GatewayIntents::GUILD_EMOJIS            |
+            GatewayIntents::GUILD_MESSAGES          |
+            GatewayIntents::GUILD_MESSAGE_REACTIONS
+        })
+        .cache_settings(|s| s.max_messages(300))
+        .await
+        .expect("Error creating client");
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ShardManagerContainer>(Arc::clone(&client.shard_manager));
+        data.insert::<PubCreds>(Arc::new(pub_creds));
+        data.insert::<ConnectionPool>(pool);
+        data.insert::<MuteMap>(Arc::new(DashMap::new()));
+        data.insert::<PrefixMap>(Arc::new(prefixes));
+        data.insert::<BotId>(bot_id);
+        data.insert::<ReqwestClient>(Arc::new(reqwest_client));
+        data.insert::<EmergencyCommands>(Arc::new(emergency_commands));
+    }
+    let shard_manager = client.shard_manager.clone();
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.expect("Failed to register Cc handler");
+        shard_manager.lock().await.shutdown_all().await;
+    });
+    if let Err(why) = client.start_shards(4).await {
+        error!("Client error: {:?}", why);
+    }
+    Ok(())
 }
